bbf2017b1cabcbb8c22437ec6e1f9fa1968718f9
gcalongi <87454626+gcalongi@users.noreply.github.com>
2023-04-28 17:26:40 -0700

    ValDataIssue
    Dealt with Matlab's ValidationData issue by removing the ValidationData argument on network save and requesting the user to select validation data % when building on existing network

--- Functions/Network Training/TrainDetNet.m
+++ Functions/Network Training/TrainDetNet.m
@@ -7,6 +7,7 @@ if isempty(TrainingTables); return; end
 choice = questdlg('Train from existing network?', 'Existing Network?', 'Yes', 'Yes - TensorFlow', 'No', 'Yes');
 switch choice
     case 'Yes'
+        detname = [];
         [NetName, NetPath] = uigetfile(handles.data.settings.networkfolder,'Select Existing Network');
         load([NetPath NetName],'detector','options');
         if (~any(strcmp(TrainingTables.Properties.VariableNames,'USV')) && detector.ClassNames==categorical({'USV'}))
@@ -20,7 +21,7 @@ switch choice
                     end
             end
         end
-        [detector, layers, options, info] = TrainSqueakDetector(TrainingTables,detector,options);
+        [detector, layers, options, info] = TrainSqueakDetector(TrainingTables,detector,options,detname);
     case 'Yes - TensorFlow'
         detector = importTensorFlowLayers(uigetdir(pwd,'Please select the folder containing saved TensorFlow 2 model (saved_model.pb & variables subfolder)'));
         [detector, layers, options, info] = TrainSqueakDetector(TrainingTables,detector);
@@ -34,6 +35,7 @@ wind = max(AllSettings(:,1));
 noverlap = max(AllSettings(:,2));
 nfft = max(AllSettings(:,3));
 imLength = max(AllSettings(:,4));
+options.ValidationData = [];
 
 version = handles.DWVersion;
 save(fullfile(PathName,FileName),'detector','layers','options','info','wind','noverlap','nfft','version','imLength');
--- Functions/Network Training/TrainSqueakDetector.m
+++ Functions/Network Training/TrainSqueakDetector.m
@@ -1,4 +1,4 @@
-function [detector, lgraph, options, info] = TrainSqueakDetector(TrainingTables, layers, sameopts)
+function [detector, lgraph, options, info] = TrainSqueakDetector(TrainingTables, layers, sameopts, detname)
 
 % Extract boxes delineations and store as boxLabelDatastore
 % Convert training and validation data to
@@ -7,57 +7,61 @@ imdsTrain = imageDatastore(TrainingTables{:,1});
 bldsTrain = boxLabelDatastore(TrainingTables(:,2:end));
 dsTrain = combine(imdsTrain,bldsTrain);
 
+list = {'Tiny YOLO v4 COCO','CSP-DarkNet-53','ResNet-50 (pre-trained)','ResNet-50 (blank)'};
 if nargin == 1
-    list = {'Tiny YOLO v4 COCO','CSP-DarkNet-53','ResNet-50 (pre-trained)','ResNet-50 (blank)'};
     [basemodels,tf] = listdlg('PromptString','Choose a base network','ListString',list,'SelectionMode','single','Name','Base Network');
     if ~tf
         return
     end
-    
-    samp = read(dsTrain);
-    if istable(samp)
-        sampleData = samp{1,1};
-        sampleImg = sampleData{1};
-    else
-        sampleData = samp(1,1);
-        sampleImg = sampleData{1};
+elseif nargin == 4
+    basemodels = find(strcmp(detname,list));
+end
+
+samp = read(dsTrain);
+if istable(samp)
+    sampleData = samp{1,1};
+    sampleImg = sampleData{1};
+else
+    sampleData = samp(1,1);
+    sampleImg = sampleData{1};
+end
+
+% Probably 300 x 300
+dim1 = size(sampleImg,1);
+dim2 = size(sampleImg,2);
+
+% Tiny - probably 288 x 288
+if basemodels == 1
+    dim1 = 32*round(size(sampleImg,1)/32);
+    dim2 = 32*round(size(sampleImg,2)/32);
+% DarkNet - probably 288 x 288 unless downsizing necessary for GPU
+elseif basemodels == 2
+    if dim1 ~= dim2
+        error('Oops, image not square, talk to Gabi')
     end
-    
-    % Probably 300 x 300
-    dim1 = size(sampleImg,1);
-    dim2 = size(sampleImg,2);
-    
-    % Tiny - probably 288 x 288
-    if basemodels == 1
-        dim1 = 32*round(size(sampleImg,1)/32);
-        dim2 = 32*round(size(sampleImg,2)/32);
-    % DarkNet - probably 288 x 288 unless downsizing necessary for GPU
-    elseif basemodels == 2
-        if dim1 ~= dim2
-            error('Oops, image not square, talk to Gabi')
-        end
-        warning('If GPU crashes, Darknet COCO may require a smaller image size.')
-        prompt = {'Enter image size (square):'};
-        dlgtitle = 'Image Size';
-        dims = [1 35];
-        definput = {num2str(dim1)};
-        dim1 = str2double(inputdlg(prompt,dlgtitle,dims,definput));
-        if mod(dim1,32) ~= 0
-            warning('COCO models require image size be a multiple of 32; automatically rounding to nearest multiple')
-            dim1 = 32*round(dim1/32);
-        end
-        dim2 = dim1;
-    % ResNet50 (pre-trained) 224 x 224
-    elseif basemodels == 3
-        dim1 = 224;
-        dim2 = 224;
+    warning('If GPU crashes, Darknet COCO may require a smaller image size.')
+    prompt = {'Enter image size (square):'};
+    dlgtitle = 'Image Size';
+    dims = [1 35];
+    definput = {num2str(dim1)};
+    dim1 = str2double(inputdlg(prompt,dlgtitle,dims,definput));
+    if mod(dim1,32) ~= 0
+        warning('COCO models require image size be a multiple of 32; automatically rounding to nearest multiple')
+        dim1 = 32*round(dim1/32);
     end
-    
-    % Training image dims need to matchcase 'Tiny YOLO v4 COCO' or
-    % 'CSP-DarkNet-53'
-    inputSize = [dim1 dim2 3];
-    dsTrainReSize = transform(dsTrain,@(data)preprocessData(data,inputSize));
-    
+    dim2 = dim1;
+% ResNet50 (pre-trained) 224 x 224
+elseif basemodels == 3
+    dim1 = 224;
+    dim2 = 224;
+end
+
+% Training image dims need to matchcase 'Tiny YOLO v4 COCO' or
+% 'CSP-DarkNet-53'
+inputSize = [dim1 dim2 3];
+dsTrainReSize = transform(dsTrain,@(data)preprocessData(data,inputSize));
+
+if nargin == 1
     %% Set training options
     bCustomize = questdlg('Would you like to customize your network options or use defaults?','Customize?','Customize','Defaults','Defaults');
     switch bCustomize
@@ -190,64 +194,7 @@ if nargin == 1
                 % labels, so may need to give up (may need to
                 % replace/supplement with a user-selected set of data)
                 case 'Yes'
-                    % Have user supply %
-                    valprop = inputdlg('What proportion of your training data would you like to allocate for validation (default = 0.1)?:','Validation Data Proportion');
-                    valprop = str2double(valprop{1});
-                    if valprop <= 0 || valprop >= 1
-                        msgbox('Improper validation % - proceeding without validation data')
-                        dsVal = [];
-                    else
-                        % Get the indices & count of each label in TrainingTables
-                        indLabs = table2cell(TrainingTables(:,2:end));
-                        indLabs = ~cellfun(@isempty,indLabs);
-                        numEachLabs = sum(indLabs,1);
-                        % Find the # of data to select based on 10% of the
-                        % whichever label has the smallest representation in the
-                        % data, but must be at least 1
-                        num2select = max(1,floor(min(valprop*numEachLabs)));
-                        % Set order of label selection from min to max
-                        % representation
-                        [~,ordLab] = sort(numEachLabs);
-                        indSel = false(size(indLabs,1),1);
-                        for i = 1:length(numEachLabs)
-                            % Amt to select from this label, accounting for
-                            % representation pulled from previous iterations of
-                            % this for loop
-                            numThisSelect = num2select - sum(indSel & indLabs(:,i));
-                            % Get the indices of data rows containing this label
-                            thisColInd = find(indLabs(:,ordLab(i)));
-                            % Randomly select num2select indices for valdata
-                            indSel(randsample(thisColInd,numThisSelect)) = true;
-                        end
-                        % Calculate proportion of each label represented in
-                        % validation data
-                        propSel = sum(indSel & indLabs)./numEachLabs;
-                        dispInfo = [TrainingTables.Properties.VariableNames(2:end);num2cell(propSel*100)];
-                        dispInfo = sprintf('%s: %0.1f%% ',dispInfo{:});
-                        answer = questdlg({'Here are the proportions corresponding to each label selected for validation:';...
-                            dispInfo; 'Do you wish to proceed?'}, ...
-                            'Check Proportions', ...
-                            'Yes','No','Yes');
-                        switch answer
-                            case 'No'
-                                return
-                            case 'Yes'
-                                valTT = TrainingTables(indSel,:);
-                                TrainingTables = TrainingTables(~indSel,:);
-                                
-                                % Convert training and validation data to
-                                % datastores for dumb YOLO fn
-                                imdsTrain = imageDatastore(TrainingTables{:,1});
-                                bldsTrain = boxLabelDatastore(TrainingTables(:,2:end));
-                                dsTrain = combine(imdsTrain,bldsTrain);
-                                dsTrainReSize = transform(dsTrain,@(data)preprocessData(data,inputSize));
-                                
-                                imdsVal = imageDatastore(valTT{:,1});
-                                bldsVal = boxLabelDatastore(valTT(:,2:end));
-                                dsVal = combine(imdsVal,bldsVal);   
-                                dsValReSize = transform(dsVal,@(data)preprocessData(data,inputSize));                     
-                        end
-                    end
+                    splitValData(TrainingTables,inputSize);
                 case 'No'
                     dsValReSize = [];
                     %Not sure why this is here so commenting until I'm forced
@@ -267,37 +214,7 @@ if nargin == 1
                       'BatchNormalizationStatistics','moving', ... %YOLOv4
                       'ResetInputNormalization',false, ... %YOLOv4
                       'Plots','training-progress');
-    end
-    
-    % % Load unweighted mobilnetV2 to modify for a YOLO net
-    % load('BlankNet.mat');
-    % 
-    % % YOLO Network Options
-    % featureExtractionLayer = "block_12_add";
-    % filterSize = [3 3];
-    % numFilters = 96;
-    % numClasses = (width(TrainingTables)-1);
-    % numAnchors = size(anchorBoxes,1);
-    % numPredictionsPerAnchor = 5;
-    % numFiltersInLastConvLayer = numAnchors*(numClasses+numPredictionsPerAnchor);
-    % 
-    % % YOLO v2 Network Layers
-    % detectionLayers = [
-    %     convolution2dLayer(filterSize,numFilters,"Name","yolov2Conv1","Padding", "same", "WeightsInitializer",@(sz)randn(sz)*0.01)
-    %     batchNormalizationLayer("Name","yolov2Batch1")
-    %     reluLayer("Name","yolov2Relu1")
-    %     convolution2dLayer(filterSize,numFilters,"Name","yolov2Conv2","Padding", "same", "WeightsInitializer",@(sz)randn(sz)*0.01)
-    %     batchNormalizationLayer("Name","yolov2Batch2")
-    %     reluLayer("Name","yolov2Relu2")
-    %     convolution2dLayer(1,numFiltersInLastConvLayer,"Name","yolov2ClassConv",...
-    %     "WeightsInitializer", @(sz)randn(sz)*0.01)
-    %     yolov2TransformLayer(numAnchors,"Name","yolov2Transform")
-    %     yolov2OutputLayer(anchorBoxes,"Name","yolov2OutputLayer")
-    %     ];
-    % 
-    % lgraph = addLayers(blankNet,detectionLayers);
-    % lgraph = connectLayers(lgraph,featureExtractionLayer,"yolov2Conv1");
-    
+    end    
     classes = TrainingTables.Properties.VariableNames(2:end);
     
     %Compute the area of each anchor box and sort them in descending order.
@@ -359,7 +276,24 @@ if nargin == 1
     
             lgraph = yolov4ObjectDetector(dlnet,classes,anchorBoxes,DetectionNetworkSource=featureExtractionLayers,InputSize=inputSize);
     end
-elseif nargin == 3
+elseif nargin == 4
+    %% Validation Data
+    % Used to determine if network is overfitting
+    bValData = questdlg({'Would you like to use a proportion of your training data to validate (recommended to assess overfitting)?';...
+        "WARNING: Due to data type restrictions and unclear Matlab documentation, using validation data may prevent data shuffling between epochs, possibly (ironically) leading to overfitting."},...
+        'Validation Data?','Yes','No','No');
+    switch bValData
+        % Select validation data - gets complicated with multiple
+        % labels, so may need to give up (may need to
+        % replace/supplement with a user-selected set of data)
+        case 'Yes'
+            [dsTrainReSize, dsValReSize] = splitValData(TrainingTables,inputSize);
+        case 'No'
+            dsValReSize = [];
+            %Not sure why this is here so commenting until I'm forced
+            %to remember why
+            %dsTrain = TrainingTables;
+    end
     warn_msg = ['If you get the following error:\n',...
         'Error using images.dltrain.internal.dltrain>iValidateSupportedTrainingOptions\n',...
         'Unsupported value for trainingOptions BatchNormalizationStatistics Name/Value. Only moving is supported.\n',...
@@ -368,6 +302,7 @@ elseif nargin == 3
     warning(warn_msg,'should produce a helpful error with the filepath for which it is searching) and put your images back in that directory.')
     lgraph = layers;
     options = sameopts;
+    options.ValidationData = dsValReSize;
 else
      error('This should not happen')   
 end
@@ -379,7 +314,6 @@ end
 function data = preprocessData(data,targetSize)
 % Resize the images and scale the pixels to between 0 and 1. Also scale the
 % corresponding bounding boxes.
-
 for ii = 1:size(data,1)
     I = data{ii,1};
     imgSize = size(I);
@@ -393,5 +327,66 @@ for ii = 1:size(data,1)
     bboxes = bboxresize(bboxes,scale);
     
     data(ii,1:2) = {I,bboxes};
+end
+end
+
+function [dsTrainReSize, dsValReSize] = splitValData(TrainingTables,inputSize)
+% Have user supply %
+valprop = inputdlg('What proportion of your training data would you like to allocate for validation (default = 0.1)?:','Validation Data Proportion');
+valprop = str2double(valprop{1});
+if valprop <= 0 || valprop >= 1
+    msgbox('Improper validation % - proceeding without validation data')
+    dsValReSize = [];
+else
+    % Get the indices & count of each label in TrainingTables
+    indLabs = table2cell(TrainingTables(:,2:end));
+    indLabs = ~cellfun(@isempty,indLabs);
+    numEachLabs = sum(indLabs,1);
+    % Find the # of data to select based on 10% of the
+    % whichever label has the smallest representation in the
+    % data, but must be at least 1
+    num2select = max(1,floor(min(valprop*numEachLabs)));
+    % Set order of label selection from min to max
+    % representation
+    [~,ordLab] = sort(numEachLabs);
+    indSel = false(size(indLabs,1),1);
+    for i = 1:length(numEachLabs)
+        % Amt to select from this label, accounting for
+        % representation pulled from previous iterations of
+        % this for loop
+        numThisSelect = num2select - sum(indSel & indLabs(:,i));
+        % Get the indices of data rows containing this label
+        thisColInd = find(indLabs(:,ordLab(i)));
+        % Randomly select num2select indices for valdata
+        indSel(randsample(thisColInd,numThisSelect)) = true;
+    end
+    % Calculate proportion of each label represented in
+    % validation data
+    propSel = sum(indSel & indLabs)./numEachLabs;
+    dispInfo = [TrainingTables.Properties.VariableNames(2:end);num2cell(propSel*100)];
+    dispInfo = sprintf('%s: %0.1f%% ',dispInfo{:});
+    answer = questdlg({'Here are the proportions corresponding to each label selected for validation:';...
+        dispInfo; 'Do you wish to proceed?'}, ...
+        'Check Proportions', ...
+        'Yes','No','Yes');
+    switch answer
+        case 'No'
+            return
+        case 'Yes'
+            valTT = TrainingTables(indSel,:);
+            TrainingTables = TrainingTables(~indSel,:);
+            
+            % Convert training and validation data to
+            % datastores for dumb YOLO fn
+            imdsTrain = imageDatastore(TrainingTables{:,1});
+            bldsTrain = boxLabelDatastore(TrainingTables(:,2:end));
+            dsTrain = combine(imdsTrain,bldsTrain);
+            dsTrainReSize = transform(dsTrain,@(data)preprocessData(data,inputSize));
+            
+            imdsVal = imageDatastore(valTT{:,1});
+            bldsVal = boxLabelDatastore(valTT(:,2:end));
+            dsVal = combine(imdsVal,bldsVal);   
+            dsValReSize = transform(dsVal,@(data)preprocessData(data,inputSize));                     
+    end
 end
 end
