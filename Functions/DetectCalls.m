86c45f3c9e0883515a802443becc4055a66e709a
gcalongi <87454626+gcalongi@users.noreply.github.com>
2023-09-22 13:42:32 -0700

    CurrentDetSaveCheck
    When Detect Calls selected, check if there is a detection file already open and check if it needs to be saved before resetting detection output folder

--- Functions/DetectCalls.m
+++ Functions/DetectCalls.m
@@ -9,6 +9,26 @@ if isempty(handles.networkfiles)
     errordlg('No Network Selected')
     return
 end
+
+%Check if pre-existing detection file exists and has changed to save file before loading a new one.
+if ~isempty(handles.data.calls) && ~isempty(handles.current_file_id)
+    [~, ~, ~, ~, ~, modcheck] = loadCallfile(fullfile(handles.detectionfiles(handles.current_file_id).folder,  handles.current_detection_file), handles,false);
+    if ~isequal(modcheck.calls, handles.data.calls) || ~isequal(modcheck.spect, handles.data.settings.spect)
+        opts.Interpreter = 'tex';
+        opts.Default='Yes';
+        if ~isequal(modcheck.calls, handles.data.calls) 
+            saveChanges = questdlg('\color{red}\bf WARNING! \color{black} Detection file has been modified. Would you like to save changes?','Save Detection File?','Yes','No',opts);
+        elseif ~isequal(modcheck.spect, handles.data.settings.spect)
+            saveChanges = questdlg('\color{red}\bf WARNING! \color{black} Spectrogram settings have been modified. Would you like to save changes in the det file (spect variable)?','Save Detection File?','Yes','No',opts);
+        end
+        switch saveChanges
+            case 'Yes'
+                SaveSession(hObject, eventdata, handles);
+            case 'No'
+        end
+    end
+end
+
 %if exist(handles.data.settings.detectionfolder,'dir')==0
     % Find audio in folder
     path=uigetdir(handles.data.settings.detectionfolder,'Select Output Detection File Folder');
