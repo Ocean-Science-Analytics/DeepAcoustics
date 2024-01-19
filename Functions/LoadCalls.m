9af3a5e020e32c21bd4ee3033c8b7406e9550488
gcalongi <87454626+gcalongi@users.noreply.github.com>
2024-01-19 11:45:17 -0800

    AudioCheck
    Added check that pathed audio still exists when loading detections and handle if not

--- Functions/LoadCalls.m
+++ Functions/LoadCalls.m
@@ -50,6 +50,38 @@ h = waitbar(0,'Loading Calls Please wait...');
 handles.data.bAnnotate = false;
 [handles.data.calls, handles.data.settings.spect, detmetadata] = loadCallfile(fullfile(handles.detectionfiles(handles.current_file_id).folder,  handles.current_detection_file), handles,false);
 
+% Make sure audio still exists in linked locations
+uniqAud = unique({handles.data.calls.Audiodata.Filename},'stable');
+newpn = '';
+for i = 1:length(uniqAud)
+    % Get current file parts
+    [~, thisfn, thisext] = fileparts(uniqAud{i});
+    % Does the audio file exist in the current set location?
+    bExist = exist(uniqAud{i},'file');
+    % If not...
+    if ~bExist
+        % ...and we recently set a new file path, check that path for this
+        % audio file
+        if ~strcmp(newpn,'')
+            bExist = exist(fullfile(newpn,[thisfn thisext]),'file');
+        end
+        % If we're still not finding the audio file, ask user to supply new
+        % path
+        if ~bExist
+            newpn = uigetdir(handles.data.settings.audiofolder,['Select folder containing ',thisfn]);
+            % Double-check that they chose a good path
+            if ~exist(fullfile(newpn,[thisfn thisext]),'file')
+                error([thisfn ' not found in ' newpn])
+            end
+        end
+        % Replace old path with new, good path
+        indrep = find(strcmp({handles.data.calls.Audiodata.Filename},uniqAud{i}));
+        for j = indrep
+            handles.data.calls.Audiodata(j).Filename = fullfile(newpn,[thisfn thisext]);
+        end
+    end
+end
+
 if nargin < 5
     indSt = 1;
     % Get names of audio files contributing to this detection file
