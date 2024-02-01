7b29c6187f2faf95be30f4436165647554dc8878
gcalongi <87454626+gcalongi@users.noreply.github.com>
2024-01-31 17:03:06 -0800

    DebugUnsupClustError
    Debugged issue saving off clustering information during unsupervised clustering

--- Functions/Call Classification/UnsupClust.m
+++ Functions/Call Classification/UnsupClust.m
@@ -632,7 +632,7 @@ function UnsupClust(app,event)
                 UpdateCluster(ClusteringData, clustAssign, clusterName, rejected)
                 update_folders(hObject, eventdata, handles);
                 handles = guidata(hObject);  % Get newest version of handles
-                if isfield(handles,'current_detection_file')
+                if isfield(handles,'current_detection_file') && ~isempty(handles.current_detection_file)    
                     LoadCalls(hObject, eventdata, handles, true)
                 end
             case false
