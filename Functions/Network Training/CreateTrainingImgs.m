4db4fd442382389e7d853c4e0c55fa65106a801a
gcalongi <87454626+gcalongi@users.noreply.github.com>
2024-04-08 09:48:48 -0700

    ToggleNoiseOffBug
    Fixed issue with added Noise category when Noise toggle is off

--- Functions/Network Training/CreateTrainingImgs.m
+++ Functions/Network Training/CreateTrainingImgs.m
@@ -82,7 +82,9 @@ if repeats > 1
     end
 end
 
-uniqLabels = cellstr(['Noise',uniqLabels']);
+if app.TrainImgSettings.bRandNoise
+    uniqLabels = cellstr(['Noise',uniqLabels']);
+end
 TTable = array2table(zeros(0,2+length(uniqLabels)));
 TTable.Properties.VariableNames = ['bAug','imageFilename',uniqLabels];
 for k = 1:length(trainingdata)
