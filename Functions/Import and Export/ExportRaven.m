78b20bae797169bd0b0599d0e9592890186118c2
gcalongi <87454626+gcalongi@users.noreply.github.com>
2026-02-21 16:44:12 -0800

    ExpRavenSTOpt
    Add option (to test!) to export Raven as selection table rather than sound selection table

--- Functions/Import and Export/ExportRaven.m
+++ Functions/Import and Export/ExportRaven.m
@@ -1,24 +1,40 @@
 function ExportRaven(hObject, eventdata, handles)
+    expCalls = handles.data.calls;
     % Export current file as a txt file for viewing in Raven
     % http://www.birds.cornell.edu/brp/raven/RavenOverview.html
     raventable = [{'Selection'} {'View'} {'Channel'} {'Begin Time (s)'} {'End Time (s)'} {'Low Freq (Hz)'} {'High Freq (Hz)'} {'Delta Time (s)'} {'Delta Freq (Hz)'} {'Avg Power Density (dB FS)'} {'Annotation'} {'Begin Path'} {'Begin File'} {'File Offset'}];
     View = 'Spectrogram 1';
     Channel = 1;
-    for i = 1:height(handles.data.calls)
-        if handles.data.calls.Accept(i)
+    RavenTableType = questdlg('Do you want to save as Sound Selection Table or Selection Table? (If you do not understand the difference, try both and see which one works the way you expect.)','Selection Table Type',...
+        'Sound Selection Table','Selection Table','Sound Selection Table');
+    if isempty(RavenTableType); return; end
+    switch RavenTableType % Load Model
+        case 'Sound Selection Table'
+            bSST = true;
+        case 'Selection Table'
+            bSST = false;
+            [expCalls] = CreateBoxAdj(expCalls, allAudio);
+    end
+    for i = 1:height(expCalls)
+        if expCalls.Accept(i)
             Selection = i;
-            BeginTime = handles.data.calls.Box(i, 1);
-            EndTime = sum(handles.data.calls.Box(i ,[1, 3]));
-            LowFreq = handles.data.calls.Box(i, 2) * 1000;
-            HighFreq = sum(handles.data.calls.Box(i, [2, 4])) * 1000;
+            if bSST
+                BeginTime = expCalls.Box(i, 1);
+                EndTime = sum(expCalls.Box(i ,[1, 3]));
+            else
+                BeginTime = expCalls.BoxAdj(i, 1);
+                EndTime = sum(expCalls.BoxAdj(i ,[1, 3]));
+            end
+            LowFreq = expCalls.Box(i, 2) * 1000;
+            HighFreq = sum(expCalls.Box(i, [2, 4])) * 1000;
             DeltaTime = EndTime - BeginTime;
             DeltaFreq = HighFreq - LowFreq;
             AvgPwr = 1;
-            Annotation = char(handles.data.calls.Type(i));
-            BeginPath = handles.data.calls.Audiodata(i).Filename;
-            [~,BeginFile,ext] = fileparts(BeginPath);
-            BeginFile = [BeginFile ext];
-            FileOffset = handles.data.calls.Box(i, 1);
+            Annotation = char(expCalls.Type(i));
+            BeginPath = expCalls.Audiodata(i).Filename;
+            [~,thisfile,ext] = fileparts(BeginPath);
+            BeginFile = [thisfile ext];
+            FileOffset = expCalls.Box(i, 1);
             raventable = [raventable; {Selection} {View} {Channel} {BeginTime} {EndTime} {LowFreq} {HighFreq} {DeltaTime} {DeltaFreq} {AvgPwr} {Annotation} {BeginPath} {BeginFile} {FileOffset}];
         end
     end
